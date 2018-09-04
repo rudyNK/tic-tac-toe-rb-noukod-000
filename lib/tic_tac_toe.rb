272321f2fffb957ed6b8858378c264a19aad5b46
Rudy Ravelin <rudy@noukod.com>
2018-09-04 14:28:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,6 +26,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
 def valid_move?(board, input)
   input.to_i.between?(1,9) && !position_taken?(board, input.to_i-1)
 end
