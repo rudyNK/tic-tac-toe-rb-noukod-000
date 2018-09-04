aeb163677cbfe4c22da376c79f7df221c731ff5a
Rudy Ravelin <rudy@noukod.com>
2018-09-04 14:13:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,4 @@
 # Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -14,34 +9,16 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
-
-# Define won?, full?, draw?, over?, and winner below
-def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
   end
 end
-
-def full?(board)
-  board.all?{|token| token == "X" || token == "O"}
-end
-
-def draw?(board)
-  full?(board) && !won?(board)
-end
-
-def over?(board)
-  won?(board) || full?(board)
-end
-
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
-  end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -49,46 +26,50 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
+def valid_move?(board, input)
+  input.to_i.between?(1,9) && !position_taken?(board, input.to_i-1)
 end
-
-def valid_move?(board, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      true
-    end
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
-
-  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
 end
-
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+def draw?(board)
+  !won?(board) && full?(board)
+end
+def over?(board)
+  won?(board) || draw?(board)
+end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-  move(board, index)
-  display_board(board)
-else
+  if !valid_move?(board, input)
     turn(board)
   end
+  move(board, input, current_player(board))
+  display_board(board)
 end
-
-def position_taken?(board, index)
-  board[index] != " "
+def position_taken?(board, location)
+  !(board[location].nil? || board[location] == " ")
+  # Creates a stop on RSpec
+  # !(board[location].nil? || board[location] == "")
 end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
-
-# Define your play method below
-def play(board)
-  turn_count = 0
-  while turn_count < 9
-    turn(board)
-    turn_count+=1
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+def move(board, location, player = "X")
+  board[location.to_i-1] = player
+end
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
   end
 end
